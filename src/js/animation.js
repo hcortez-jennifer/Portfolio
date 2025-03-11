5aaa806d5ae106eb6806efdf1a44e866e1724614
hcortez-jennifer <117784015+hcortez-jennifer@users.noreply.github.com>
2025-03-10 22:19:58 -0400

    COMPLETE: works section

--- src/js/animation.js
+++ src/js/animation.js
@@ -33,3 +33,54 @@ gsap.timeline()
         yoyo: true,
         ease: "power1.inOut"
     })
+
+
+// WORKS ANIMATION
+function animateWorks() {
+    let scrollAnimation = gsap.to(".projects__card__container", {
+        y: () => -(
+            document.querySelector(".projects__card__container").scrollHeight -
+            document.querySelector(".projects__card__container").clientHeight
+        ),
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".projects",
+            start: "top top",
+            end: "bottom bottom",
+            pin: ".projects__info",
+            scrub: 1,
+        }
+    });
+
+    gsap.utils.toArray(".project__card").forEach((card) => {
+        gsap.fromTo(
+            card,
+            { opacity: 0, y: 100, scale: 1 }, 
+            {
+                opacity: 1,
+                y: 0,
+                scale: 1, 
+                duration: 1,
+                ease: "power2.out",
+                scrollTrigger: {
+                    trigger: card,
+                    start: "top 80%",
+                    toggleActions: "play none none reverse",
+                }
+            }
+        );
+    });
+
+    ScrollTrigger.matchMedia({
+        "(max-width: 991px)": function () {
+            scrollAnimation.kill(); 
+            ScrollTrigger.getAll().forEach(trigger => trigger.kill()); 
+            gsap.set(".projects__info", { position: "relative" }); 
+        },
+        "(min-width: 992px)": function () {
+            gsap.set(".projects__info", { position: "sticky", top: "0" }); 
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", animateWorks);
